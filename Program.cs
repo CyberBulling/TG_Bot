67ef7c5fc4a2bfda63187bc0f7933054aa14f536
Boo <zumozumo03082005@gmail.com>
2022-05-19 19:53:20 +0800

    2.1

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 using Telegram.Bot;
 using Telegram.Bot.Extensions.Polling;
 using Telegram.Bot.Types;
@@ -9,10 +10,11 @@ namespace TelegramBotExperiments
 {
 	class Program
 	{
-		static ITelegramBotClient bot = new TelegramBotClient("5150375839:AAED-xI8lx6XaZjtEFXILpCcDYtVrNZtKsU");
+		static ITelegramBotClient bot = new TelegramBotClient("5365199670:AAEt_XClVQH_oCJpsQ1PO6nZqdErZuqWDLw");
+		private static string joke;
+		private static GetRequest request = new GetRequest("https://geek-jokes.sameerkumar.website/api?format=json");
 		public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
 		{
-			// Некоторые действия
 			Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(update));
 			if (update.Type == Telegram.Bot.Types.Enums.UpdateType.Message)
 			{
@@ -22,24 +24,32 @@ namespace TelegramBotExperiments
 					await botClient.SendTextMessageAsync(message.Chat, "Добро пожаловать на борт, добрый путник!");
 					return;
 				}
-				if (message.Text.ToLower() == "stic")
-				{
-					await botClient.SendStickerAsync(message.Chat, "https://tlgrm.ru/_/stickers/ccd/a8d/ccda8d5d-d492-4393-8bb7-e33f77c24907/1.webp");
-				}
-				else if (message.Text.ToLower() == "hi" || message.Text.ToLower() == "hello" || message.Text.ToLower() == "привет")
+				else if (message.Text.ToLower() == "hi" || message.Text.ToLower() == "привет")
 				{
 					await botClient.SendTextMessageAsync(message.Chat, "Hello");
 				}
+				else if (message.Text.ToLower() == "--help")
+				{
+					await botClient.SendTextMessageAsync(message.Chat, $"1./start \n2.hi / привет \n3./joke ");
+				}
+				else if (message.Text.ToLower() == "/joke")
+				{
+					request.Run();
+					var response = request.Response;
+					var json = JObject.Parse(response);
+					joke = json["joke"].ToString();
+					await botClient.SendTextMessageAsync(message.Chat, joke);
+				}
 				else
 				{
-					await botClient.SendTextMessageAsync(message.Chat, "");
+					await botClient.SendTextMessageAsync(message.Chat, "if you need help use: --help");
 				}
 			}
 		}
-		//-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+		//---------------------------------------------------------------------------------------------------------------------------------------------- 
+
 		public static Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
 		{
-			// Некоторые действия
 			Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(exception));
 			return Task.CompletedTask;
 		}
@@ -47,18 +57,17 @@ namespace TelegramBotExperiments
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Запущен бот " + bot.GetMeAsync().Result.FirstName);
-
 			var cts = new CancellationTokenSource();
 			var cancellationToken = cts.Token;
 			var receiverOptions = new ReceiverOptions
 			{
-				AllowedUpdates = { }, // receive all update types
+				AllowedUpdates = { },
 			};
 			bot.StartReceiving(
-				HandleUpdateAsync,
-				HandleErrorAsync,
-				receiverOptions,
-				cancellationToken
+			 HandleUpdateAsync,
+			 HandleErrorAsync,
+			 receiverOptions,
+			 cancellationToken
 			);
 			Console.ReadLine();
 		}
